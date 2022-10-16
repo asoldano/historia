6b507c44b6e7d8c377bfdc28c034a5987a59eb2c
Alessio Soldano <asoldano@redhat.com>
2022-10-17 00:10:37 +0200

    Preparing for using vega-lite-api for data visualization

--- core/src/main/java/org/jboss/historia/core/UntestedCommitDetectionStrategy.java
+++ core/src/main/java/org/jboss/historia/core/UntestedCommitDetectionStrategy.java
@@ -1,5 +1,7 @@
 package org.jboss.historia.core;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -12,7 +14,7 @@ public class UntestedCommitDetectionStrategy {
 	
 	private static final Logger LOGGER = Logger.getLogger(UntestedCommitDetectionStrategy.class);
 
-	public class FileUpdates {
+	public static class FileUpdates {
 		private final String path;
 		private long updates = 0;
 		private long untestedUpdates = 0;
@@ -44,50 +46,73 @@ public class UntestedCommitDetectionStrategy {
 		public String toString() {
 			return path + "(Untested: " + untestedUpdates + "/" + updates + ")";
 		}
+		
+		public static void printHeader(Writer w) throws IOException {
+			w.append("File path").append(",").append("# updates").append(",").append("# untested updates").append("\n");
+		}
+		
+		public void print(Writer w) throws IOException {
+			w.append(path).append(",").append(String.valueOf(updates)).append(",").append(String.valueOf(untestedUpdates)).append("\n");
+		}
 	}
 	
-	private final String pathPrefixFilter;
+	private final String pathFilter;
+	private final boolean isPrefix;
 	
-	public UntestedCommitDetectionStrategy() {
-		this.pathPrefixFilter = null;
-	}
-	
-	public UntestedCommitDetectionStrategy(String pathPrefixFilter) {
-		this.pathPrefixFilter = pathPrefixFilter;
+	public UntestedCommitDetectionStrategy(String pathFilter, boolean isPrefix) {
+		this.pathFilter = pathFilter;
+		this.isPrefix = isPrefix;
 	}
 	
 	public List<FileUpdates> process(String repositoryUri, String localRepoCloneURI) throws Exception {
 		JGitUtils jgit = new JGitUtils(repositoryUri, localRepoCloneURI);
 		Set<String> files = jgit.getFilesOnHEAD();
 		List<FileUpdates> list = new ArrayList<>();
-		boolean debug = LOGGER.isDebugEnabled();
 		for (String f : files) {
-			if (pathPrefixFilter == null || f.startsWith(pathPrefixFilter)) {
-				if (debug)
-					LOGGER.debug("F: " + f);
+			if (pathFilter == null || (isPrefix ? f.startsWith(pathFilter) : f.contains(pathFilter))) {
 				FileUpdates fu = new FileUpdates(f);
+				processFile(fu, jgit, f, LOGGER.isDebugEnabled());
 				list.add(fu);
-				List<RevCommit> commitHistory = jgit.getFileHistory(f);
-				int s = commitHistory.size();
-				for (int i = 0; i < s - 1; i++) {
-					RevCommit commit = commitHistory.get(i);
-					if (debug)
-						LOGGER.debug(" Commit #" + (i+1) + "- " + commit.getName() +": "+ commit.getShortMessage());
-					fu.incrementUpdates();
-					if (!affectsTests(jgit, fu, jgit.getParentCommitTree(commit), commit.getTree())) {
-						fu.incrementUntestedUpdates();
-					}
-				}
-				fu.incrementUpdates();
-				if (debug)
-					LOGGER.debug(" Commit #" + s + ": " + commitHistory.get(s - 1).getShortMessage());
-				if (!affectsTests(jgit, fu, null, commitHistory.get(s - 1).getTree())) {
-					fu.incrementUntestedUpdates();
-				}
 			}
 		}
 		jgit.close();
 		return list;
+	}
+	
+	public void process(String repositoryUri, String localRepoCloneURI, Writer w) throws Exception {
+		JGitUtils jgit = new JGitUtils(repositoryUri, localRepoCloneURI);
+		Set<String> files = jgit.getFilesOnHEAD();
+		FileUpdates.printHeader(w);
+		for (String f : files) {
+			if (pathFilter == null || (isPrefix ? f.startsWith(pathFilter) : f.contains(pathFilter))) {
+				FileUpdates fu = new FileUpdates(f);
+				processFile(fu, jgit, f, LOGGER.isDebugEnabled());
+				fu.print(w);
+			}
+		}
+		jgit.close();
+	}
+	
+	private void processFile(FileUpdates fu, JGitUtils jgit, String f, boolean debug) throws Exception {
+		if (debug)
+			LOGGER.debug("F: " + f);
+		List<RevCommit> commitHistory = jgit.getFileHistory(f);
+		int s = commitHistory.size();
+		for (int i = 0; i < s - 1; i++) {
+			RevCommit commit = commitHistory.get(i);
+			if (debug)
+				LOGGER.debug(" Commit #" + (i + 1) + "- " + commit.getName() + ": " + commit.getShortMessage());
+			fu.incrementUpdates();
+			if (!affectsTests(jgit, fu, jgit.getParentCommitTree(commit), commit.getTree())) {
+				fu.incrementUntestedUpdates();
+			}
+		}
+		fu.incrementUpdates();
+		if (debug)
+			LOGGER.debug(" Commit #" + s + ": " + commitHistory.get(s - 1).getShortMessage());
+		if (!affectsTests(jgit, fu, null, commitHistory.get(s - 1).getTree())) {
+			fu.incrementUntestedUpdates();
+		}
 	}
 	
 	private boolean affectsTests(JGitUtils jgit, FileUpdates fu, RevTree parentCommitTree, RevTree commitTree) throws Exception {
@@ -101,7 +126,7 @@ public class UntestedCommitDetectionStrategy {
 		for (String cf : cfs) {
 			if (debug)
 				LOGGER.debug("    " + cf);
-			if (cf.startsWith("src/test")) {
+			if (cf.contains("src/test")) {
 				return true;
 			}
 		}
--- core/src/test/java/org/jboss/historia/core/UntestedCommitDetectionStrategyTest.java
+++ core/src/test/java/org/jboss/historia/core/UntestedCommitDetectionStrategyTest.java
@@ -1,20 +1,48 @@
 package org.jboss.historia.core;
 
-import java.util.List;
+import static org.junit.Assert.assertTrue;
 
-import org.apache.log4j.Logger;
-import org.jboss.historia.core.UntestedCommitDetectionStrategy.FileUpdates;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 import org.junit.Test;
 
 public class UntestedCommitDetectionStrategyTest {
 	
 	@Test
 	public void testProcessing() throws Exception {
-		Logger LOGGER = Logger.getLogger(this.getClass());
-		UntestedCommitDetectionStrategy s = new UntestedCommitDetectionStrategy("src/main/java");
-		List<FileUpdates> list = s.process("https://github.com/jbossws/jbossws-spi.git", "target/jgit/jbossws-spi");
-		for (FileUpdates f : list) {
-			LOGGER.info(f);
+		final String filename = "target/test-processing-jbossws-spi-" + System.currentTimeMillis() + ".csv";
+		UntestedCommitDetectionStrategy s = new UntestedCommitDetectionStrategy("src/main/java", true);
+		try (FileWriter writer = new FileWriter(filename, true); BufferedWriter bw = new BufferedWriter(writer)) {
+			s.process("https://github.com/jbossws/jbossws-spi.git", "target/jgit/jbossws-spi", bw);
 		}
+		boolean found = false;
+		try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+			String line = reader.readLine();
+			while (line != null && !found) {
+				found = reader.readLine().contains("src/main/java/org/jboss/wsf/spi/deployment/Extensible.java,5,5");
+			}
+		}
+		assertTrue(found);
 	}
+
+//	@Test
+//	public void testProcessing2() throws Exception {
+//		final String filename = "target/test-processing-resteasy-" + System.currentTimeMillis() + ".csv";
+//		UntestedCommitDetectionStrategy s = new UntestedCommitDetectionStrategy("src/main/java", false);
+//		try (FileWriter writer = new FileWriter(filename, true); BufferedWriter bw = new BufferedWriter(writer)) {
+//			s.process("https://github.com/resteasy/resteasy.git", "target/jgit/resteasy", bw);
+//		}
+////		boolean found = false;
+////		try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+////			String line = reader.readLine();
+////			while (line != null && !found) {
+////				found = reader.readLine().contains("src/main/java/org/jboss/wsf/spi/deployment/Extensible.java,5,5");
+////			}
+////		}
+////		assertTrue(found);
+//	}
+
 }
